0912df7e4152454063e1478b36c0a37022ad6831
ezzat walid <ew1139@fayoum.edu.eg>
2025-07-28 23:27:46 +0300

    Update Mo's.cpp

--- DataStructure/Mo's.cpp
+++ DataStructure/Mo's.cpp
@@ -2,30 +2,37 @@
 // Created by Ezzat on 6/2/2025.
 //
 struct query {
-    int l, r, idx;
-
-    bool operator<(const query &other) {
-        return make_pair(l / SQ, r) < make_pair(other.l / SQ, other.r);
+    ll l , r , idx ;
+    bool operator<( query& q) {
+        if (l / sq != q.l / sq )return l / sq < q.l / sq;
+        return (l / sq) & 1 ? r > q.r : r < q.r;
     }
 };
 
 void code() {
-   uto del = [&](ll idx) {
-        // to do
+    vector<query> Q(q);
+    for (int i = 0; i < q; ++i) {
+        auto &[l, r, idx] = Q[i];
+        cin >> l >> r, l--, r--, idx = i;
+    }
+
+    ll l = 1, r = 0, sum = 0;
+    auto add = [&](ll idx) {
+
     };
-    auto query = [&]() {
-        // to do
+    auto del = [&](ll idx) {
+
     };
     std::sort(Q.begin(), Q.end());
+    vector<int> ans(q);
     for (auto [Ql, Qr, idx]: Q) {
         while (l > Ql)add(--l);
-        while (l < Ql)del(l++);
         while (r < Qr)add(++r);
+        while (l < Ql)del(l++);
         while (r > Qr)del(r--);
-        ans[idx] = query();
-    } ll l = 1, r = 0, sum = 0;
-    auto add = [&](ll idx) {
-        // to do
-    };
-    a
+        ans[idx] = sum;
+    }
+    for (auto i: ans)
+        cout << i << ' ';
+    cout << '\n';
 }
